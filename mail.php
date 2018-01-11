32aeaab8b4f5ac3904b783a8eb2b273ba024d695
atag00 <ivasyuk7@gmail.com>
2018-01-11 21:19:13 +0200

    some fixes

--- mail.php
+++ mail.php
@@ -9,3 +9,4 @@ $message = "Імя: $name \nТелефон: $phone";
 
 $pagetitle = "Нова заявка з сайту \"$sitename\"";
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
+?>
